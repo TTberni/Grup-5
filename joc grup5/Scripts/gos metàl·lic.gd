df63930ba41737d2c6e89e6a8c466d2cf8cbf471
TTberni <bernatvillena@gmail.com>
2020-05-13 20:25:13 +0200

    Gos metàl·lic acabat

--- joc grup5/Scripts/gos metàl·lic.gd
+++ joc grup5/Scripts/gos metàl·lic.gd
@@ -3,20 +3,16 @@ var constant = 300
 
 signal cop
 var vel_max = 0 
-func _on_gos_metllic_body_entered(body,c ):
-	constant *=  -1
-
 func _process(delta):
-	var c = vel_max + constant 
-	var velocitat = mou(c)
-	anima(velocitat)
-	return c
-func mou(c):
-	
-	set_linear_velocity(Vector2(c, 0))
-	var velocitat = c
-	return velocitat
-
+	mou()
+func mou():
+	 $"..".offset += 2
+	 if $"..".offset >= 534.74:
+		 $AnimatedSprite.flip_h = true
+	 else:
+		 $AnimatedSprite.flip_h = false
+		
+		
 
 func anima(velocitat):
 	if velocitat < 0:
