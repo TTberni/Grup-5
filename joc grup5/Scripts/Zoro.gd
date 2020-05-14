39ad46e137605bd1f1c1778fd12fc5a5d6b441de
TTberni <bernatvillena@gmail.com>
2020-05-14 21:48:04 +0200

    canvis

--- joc grup5/Scripts/Zoro.gd
+++ joc grup5/Scripts/Zoro.gd
@@ -3,8 +3,10 @@ extends KinematicBody2D
 var velocitat = Vector2.ZERO
 var vel_max = 200
 var gravetat = Vector2(0, 450)
+signal atac
 
-
+func _ready():
+	$Area2D/CollisionShape2D.disabled
 func _process(delta):
 	var moviment = mou(delta)
 	anima(moviment)
@@ -23,29 +25,37 @@ func mou(delta):
 		velocitat.x += -vel_max
 	if Input.is_action_pressed("salta") and is_on_floor():
 		velocitat.y += -300
-		$AnimatedSprite.play("salta")
 	var moviment = move_and_slide(velocitat, Vector2.UP)
 	return moviment
+
 		
 func anima(moviment):
+	if Input.is_action_pressed("atacar"): 
+		$Area2D/CollisionShape2D.is_one_way_collision_enabled()
+		$AnimatedSprite.play("ataca")
 	
-	if moviment.y < 0  :
+	if moviment.y < 0 and moviment.x > 0:
+		$AnimatedSprite.flip_h = true
 		$AnimatedSprite.play("salta")
-	
-	if moviment.x == 0 and moviment.y == 0:
-		$AnimatedSprite.play("espera")
 		
-	if moviment.x < 0 and is_on_floor():
+	if moviment.y < 0 and moviment.x < 0:
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("salta")
+	if moviment.x == 0 and is_on_floor():
+		$AnimatedSprite.play("espera")
+	
+	if  moviment.x < 0  and is_on_floor():
 		$AnimatedSprite.play("corrents")
 		$AnimatedSprite.flip_h = false
 		
-	if moviment.x > 0 and is_on_floor():
+	if moviment.x > 0  and is_on_floor():
 		$AnimatedSprite.play("corrents")
 		$AnimatedSprite.flip_h = true
 	
-	
 
-
+		
+func _on_Area2D_area_entered(area):
+	emit_signal("atac")
 
 
 
