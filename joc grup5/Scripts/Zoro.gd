21d8b47ef766c95c1ea77f2731c2148ca6b9e228
TTberni <bernatvillena@gmail.com>
2020-05-26 22:50:00 +0200

    canviant collisio i altres baïnes

--- joc grup5/Scripts/Zoro.gd
+++ joc grup5/Scripts/Zoro.gd
@@ -10,16 +10,15 @@ signal cop
 
 func _ready():
 	$"rang atac/CollisionShape2D".disabled
-	$corre.disabled = true
+	
 #	automat =$AnimationTree.get("parameters/playback")
 #	$Sprites/ataca.hide()
 #	$Sprites/corre.hide()
-#	$Sprites/mort.hide()
 #	$Sprites/salta.hide()
 #	$"rang atac/CollisionShape2D".disabled
 func _process(delta):
 	var moviment = mou(delta)
-	anima(moviment)
+	
 
 func mou(delta):
 	velocitat.x = 0
@@ -43,41 +42,10 @@ func mou(delta):
 	return moviment
 
 		
-func anima(moviment):
-	
-	
-	if moviment.y < 0 and moviment.x > 0:
-		$corre.disabled = true
-		$espera.disabled = false
-		$AnimatedSprite.play("salta")
-		$AnimatedSprite.flip_h = true
-		
-		
-	if moviment.y < 0 and moviment.x < 0:
-		$espera.disabled =false
-		$corre.disabled = true
-		$AnimatedSprite.flip_h = false
-		$AnimatedSprite.play("salta")
-		
-	if moviment.x == 0 and is_on_floor():
-		$corre.disabled = true
-		$espera.disabled = false
-		$AnimatedSprite.play("espera")
-	
-	if  moviment.x < 0  and is_on_floor():
-		$espera.disabled = true
-		$corre.disabled = false
-		$AnimatedSprite.play("corre")
-		$AnimatedSprite.flip_h = false
-		
-	if moviment.x > 0 and is_on_floor():
-		$espera.disabled = true
-		$corre.disabled = false
-		$AnimatedSprite.play("corre")
-		$AnimatedSprite.flip_h = true
+
 	
 
 		
 
-func mor():
-	$AnimatedSprite.play("mort")
+
+
