754764d59b83f3c8ad2e86bf856370140f25e928
TTberni <bernatvillena@gmail.com>
2020-06-04 14:50:31 +0200

    millorant a zoro

--- joc grup5/Scripts/Zoro.gd
+++ joc grup5/Scripts/Zoro.gd
@@ -14,8 +14,10 @@ func _ready():
 #	
 func _process(delta):
 	var moviment = mou(delta)
-
-
+	if moviment.x == 0 and is_on_floor():
+		automat.travel("espera")
+		
+	
 func mou(delta):
 	velocitat.x = 0
 	
@@ -23,22 +25,24 @@ func mou(delta):
 		velocitat.y = 0
 	else:
 		velocitat += gravetat*delta
-		
-		
+	
 	if Input.is_action_pressed("dreta"):
 		velocitat.x += vel_max
-		automat.travel('corre')
+		automat.travel("corre")
+		scale.x = -0.5
+		
 	if Input.is_action_pressed("esquerra"):
 		velocitat.x += -vel_max
-		automat.travel('corre')
+		automat.travel("corre")
+		scale.x = scale.x *-1
+		
 	if Input.is_action_pressed("salta") and is_on_floor():
 		velocitat = + salt
-		automat.travel('salta')
+		automat.travel("salta")
 	if Input.is_action_pressed("atacar"): 
-		$"rang atac/CollisionShape2D".disabled = false
 		automat.travel("atac")
+		
 	var moviment = move_and_slide(velocitat, Vector2.UP)
 	return moviment
 
-
-
+	
